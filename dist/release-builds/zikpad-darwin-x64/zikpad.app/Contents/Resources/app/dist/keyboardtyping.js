f8fe1d10665a4152398d0d5353db9e4e478274a1
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2021-04-16 23:38:53 +0200

    new version

--- dist/release-builds/zikpad-darwin-x64/zikpad.app/Contents/Resources/app/dist/keyboardtyping.js
+++
@@ -1,5 +0,0 @@
-const ipc = require('electron').ipcRenderer;
-document.getElementById("validate").addEventListener("click", () => {
-    ipc.send("typing", document.getElementById("lilypond").value);
-});
-//# sourceMappingURL=keyboardtyping.js.map
